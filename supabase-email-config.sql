d27e793d44a16fd91c187c234087b504c398d38f
JK-Beast <manickavasagan022@gmail.com>
2025-10-17 19:46:42 +0530

    Remove unused dependencies and files

--- supabase-email-config.sql
+++
@@ -1,4 +0,0 @@
--- Add email configuration columns to profiles table
-ALTER TABLE profiles ADD COLUMN email_provider VARCHAR(50);
-ALTER TABLE profiles ADD COLUMN encrypted_email_password TEXT;
-ALTER TABLE profiles ADD COLUMN email_configured BOOLEAN DEFAULT FALSE;
